c415cf91b3d6b60368612f27155a4c9a4ab5fffd
Martin <martin.ganahl@gmail.com>
2019-08-01 15:21:43 -0400

    moved some functions into nummpy_utils

--- experiments/MPS/positivize.py
+++ experiments/MPS/positivize.py
@@ -12,8 +12,6 @@ import experiments.MPS_classifier.MPSMNIST as mm
 import experiments.MPS.overlap_minimizer as OM
 from scipy.linalg import expm
 import numpy as  np
-from lib.mpslib.Tensor import Tensor
-import lib.mpslib.TensorNetwork as TN
 import copy
 import os
 import itertools
@@ -24,57 +22,7 @@ import sign_mps_data.readMPS as readMPS
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 misc_mps.compile_ncon(True)
-
 plt.ion()
-
-def generate_basis(N):
-    basis = []
-    for n in range(2**N):
-        text =  '{:0' + str(N) + 'b}'
-        basis.append(np.array([int(t) for t in text.format(n)]))
-    basis = np.stack(basis,axis=0)
-    return basis
-
-
-def get_product_states(sigmas,d):
-    mpss = []
-    N = sigmas.shape[1]
-    for m in  range(sigmas.shape[0]):
-        tensors = [np.zeros((1,1,d)).view(Tensor) for _ in range(sigmas.shape[1])]
-        for n in  range(len(tensors)):
-            tensors[n][0,0,sigmas[m,n]] = 1
-        mpss.append(TN.FiniteMPS(tensors))
-    return mpss
-
-def get_sample_mps(mps, num_samples,Dmax):
-    """
-    compute the mps obtained from sampling `num_samples` states from `mps` and 
-    taking an equal superposition of all 
-    the use of this in OverlapMinimizer is deprecated; OverlapMinimizer can directly optimize from a 
-    list of samples
-    Args:
-        mps (FiniteMPSCentralGauge)
-        num_samples (int): number of samples to draw from `mps`
-        Dmax (int):  the maximum bond dimension to be kept when building the sampled mps
-    Returns:
-        FiniteMPSCentralGauge
-    """
-    if not Dmax:
-        Dmax=40
-    samples = mps.generate_samples(num_samples)
-    mpss=get_product_states(samples.numpy(), 2)
-    for m in range(1,len(mpss)):
-        mpss[0] = mpss[0] + mpss[m]
-        if m % Dmax == 0:
-            mpss[0].position(0)
-            mpss[0].position(len(mpss[0]))
-            mpss[0].position(0,D=Dmax)
-            #print(mpss[0].D)
-    tensors = [tf.convert_to_tensor(np.transpose(mpss[0].get_tensor(n),(0,2,1))) for n in  range(len(mpss[0]))]
-    mps = MPS.FiniteMPSCentralGauge(tensors=tensors)
-    mps.position(0) #normalize the state
-    mps.position(len(mps),normalize=True,D=Dmax)
-    return mps
 
 def apply_random_positive_gates(mps):
     for site in range(0,len(mps)-1,2):
@@ -88,25 +36,17 @@ def apply_random_positive_gates(mps):
     mps.position(0)
     return mps
 
-def read_and_convert_to_pyten(prefix,dtype=tf.float64):
+
+def convert_to_tf(tensors):
     """
-    function to read Giacomo's mps data
+    permutes the indices of the tensors from PyTeN ordering to TensorNetwork ordering
     Args: 
-         prefix (str):  the path + the prefix to  the data; the routine appends
-                        '_tensors.txt' and '_index.txt' to `prefix`
+         tensors (list of np.ndarrau):  
+
     Returns:
-        list of PyTen.lib.mpslib.Tensor.Tensor objects (essentially np.ndarrays)
+        list of tf.Tensor objects
     """
-    tensors = readMPS.readMPS(prefix + '_tensors.txt',prefix + '_index.txt',N=8,convert=True)
-    tensors_new=[]
-    for n in range(len(tensors)):
-        if n ==0:
-            tensors_new.append(np.transpose(np.expand_dims(tensors[n],0),(0,2,1)).astype(dtype.as_numpy_dtype))
-        elif n > 0 and n < len(tensors) - 1:
-            tensors_new.append(np.transpose(tensors[n], (0,2,1)).astype(dtype.as_numpy_dtype))
-        elif n == len(tensors) -1 :
-            tensors_new.append(np.transpose(np.expand_dims(tensors[n],2), (1,2,0)).astype(dtype.as_numpy_dtype))
-    return [t.view(Tensor) for t in tensors_new]
+    return [tf.transpose(tf.convert_to_tensor(tensors[n]),(0,2,1)) for n in range(len(tensors))] #the index order of the PyTeN and TensorNetwork MPS is different
 
 def read_and_convert_to_tf(prefix,  dtype=tf.float64):
     """
@@ -119,6 +59,7 @@ def read_and_convert_to_tf(prefix,  dtype=tf.float64):
     """
     tensors = read_and_convert_to_pyten(prefix,dtype)
     return [tf.transpose(tf.convert_to_tensor(tensors[n]),(0,2,1)) for n in range(len(tensors))] #the index order of the PyTeN and TensorNetwork MPS is different
+
 
 #%matplotlib qt
 def analyze_positivity(mps, samples=None, nsamples=1000, show=True, fignum=0, bins=100, verbose=0):
@@ -180,24 +121,15 @@ def analyze_positivity(mps, samples=None, nsamples=1000, show=True, fignum=0, bi
             plt.clf()            
             ax = plt.subplot(2,1,1)
             plt.title(r'log-amplitudes; min ($\log$(amps))={0}, max($\log$(amp))={1}'.format(np.round(np.min(log_amps), 2),np.round(np.max(log_amps), 2)))
-            #plt.hist(np.imag(phases * log_amps), bins=bins)                        
-            #plt.hist(-np.sign(np.real(phases)) * np.real(log_amps*phases), bins=bins)
             plt.hist(np.real(np.exp(log_amps)*phases), bins=bins)
             plt.xlabel(r'$\Re(amp)$')                        
-            #plt.xlabel(r'-Sign($\Re$(amp)) $\log (\vert$amp$\vert)$')
             plt.ylabel('frequency')
-            #plt.xlim([min(np.real(np.exp(log_amps)*phases)),max(np.real(np.exp(log_amps)*phases))])            
-            #ax.set_xscale('log')
+
             ax = plt.subplot(2,1,2)
-            #plt.hist(-np.sign(np.imag(phases)) * np.imag(log_amps*phases), bins=bins)
             plt.hist(np.imag(np.exp(log_amps)*phases), bins=bins)
             plt.xlabel(r'$\Im(amp)$')            
-            #plt.hist(np.imag(phases * log_amps), bins=bins)            
-            #plt.xlabel(r'-Sign($\Im$(amp)) $\log (\vert$amp$\vert)$')            
             plt.ylabel('frequency')
-            #ax.set_xscale('log')            
             plt.tight_layout()
-            #plt.xlim([min(np.imag(np.exp(log_amps)*phases)),max(np.imag(np.exp(log_amps)*phases))])
             plt.draw()
             plt.show()
             plt.pause(0.01)        
@@ -205,7 +137,7 @@ def analyze_positivity(mps, samples=None, nsamples=1000, show=True, fignum=0, bi
         av_real = np.mean(np.real(np.exp(log_amps)*phases))
         av_imag = np.mean(np.imag(np.exp(log_amps)*phases))
         av_abs_imag = np.mean(np.abs(np.imag(np.exp(log_amps)*phases)))
-        #av_sign = np.mean(np.sign(np.real(phases))) + 1j * np.mean(np.sign(np.imag(phases)))
+
         print()
         print()
         print('#############################')
@@ -220,11 +152,7 @@ def analyze_positivity(mps, samples=None, nsamples=1000, show=True, fignum=0, bi
         return av_real_sign, av_abs_imag
 
     
-def equal_superposition(ds,dtype=np.float64):
-    """
-    return mps tensors correpsonding to the equal superposition of all basis states
-    """
-    return [np.expand_dims(np.expand_dims(np.ones(ds[n]), 0),0).astype(dtype).view(Tensor) for n in range(len(ds))]
+
 def equal_superposition_tf(ds,dtype=tf.float64):
     """
     return mps tensors correpsonding to the equal superposition of all basis states
@@ -314,49 +242,3 @@ def positivize_from_self_sampling(minimizer, ref_mps=None, Dmax=20,num_its=100, 
         
     return minimizer
 
-# def optimal_positivizer(mps_in, num_sweeps=10, num_opt_sweeps = 10, num_its=4, num_samples=100,Dmax=None,Dmax_sample=None, alpha=1.0, verbose=1):
-#     mps = copy.deepcopy(mps_in)
-#     #first converge with respect to the totally mixed state
-#     ds = mps.d
-#     gates={(site, site+1): tf.reshape(tf.eye(ds[site]*ds[site+1], dtype=dtype), 
-#                                       (ds[site], ds[site+1], ds[site], ds[site+1]))
-#             for site in range(0,N-1)}
-#     ref_mps = MPS.FiniteMPSCentralGauge(tensors=equal_superposition_tf(ds))
-#     ref_mps.position(0,normalize=True)
-#     ref_mps.position(len(ref_mps),normalize=True)  
-#     minimizer = OM.OverlapMinimizer(mps,ref_mps, gates)
-#     for _ in range(num_sweeps):
-#         minimizer.minimize_even(num_iterations=num_its,verbose=1)
-#         minimizer.minimize_odd(num_iterations=num_its,verbose=1)
-   
-#     #now the gates should be close to he ideal gates
-#     #switch to the ideal optimization scheme:
-#     for sweep in range(num_opt_sweeps):
-#         for site in range(len(minimizer.mps) - 1):
-#             #reset all envs to empty
-#             minimizer.left_envs = {}
-#             minimizer.right_envs = {}            
-#             [minimizer.add_unitary_right(pos, minimizer.right_envs, minimizer.mps, minimizer.conj_mps, minimizer.gates) for pos in reversed(range(site+1,len(minimizer.mps)))]
-#             [minimizer.add_unitary_left(pos, minimizer.left_envs, minimizer.mps, minimizer.conj_mps, minimizer.gates) for pos in range(site)]
-            
-#             env = minimizer.gates[(site, site+1)] * (1-alpha) + alpha * minimizer.get_env((site, site + 1),
-#                                                                                           minimizer.left_envs, minimizer.right_envs, minimizer.mps, minimizer.conj_mps)
-#             minimizer.gates[(site, site+1)] = minimizer.u_update_svd_numpy(env)
-#             minimizer.add_unitary_left(site,minimizer.left_envs, minimizer.mps, minimizer.conj_mps, minimizer.gates)
-#             if verbose > 0 and site > 0:
-#                 overlap = minimizer.overlap(site, minimizer.left_envs, minimizer.right_envs, minimizer.mps, minimizer.conj_mps)
-#                 stdout.write("\r site %i, overlap = %.16f" %
-#                     (site, np.abs(np.real(overlap))))
-#                 stdout.flush()
-#             if verbose > 1:
-#                 print()
-
-#             minimizer.absorb_gates() #absorb the current gates into the state 
-#             minimizer.mps.position(0) #normalize the state
-#             minimizer.mps.position(len(minimizer.mps),normalize=True)
-#             minimizer.mps.position(0,normalize=True,D=Dmax) #normalize the state
-#             analyze_positivity(minimizer.mps,nsamples=2000, show_imag=False)
-#             #minimizer = OM.OverlapMinimizer(copy.deepcopy(mps_in),get_sample_mps(minimizer.mps, num_samples) , gates)
-#             #generate a new ref state by sampling from U *minimizer.mps
-#             minimizer.conj_mps = get_sample_mps(minimizer.mps, num_samples,Dmax=Dmax_sample)     #returns normalized state
-#             minimizer.mps = copy.deepcopy(mps_in)#reset the input state
