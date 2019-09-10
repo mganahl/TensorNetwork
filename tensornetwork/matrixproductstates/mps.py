5029fea3514da0b1ef284e07b71c570549d2ff5d
mganahl <mganahl@pitp.ca>
2019-09-10 14:04:53 -0400

    removed redundant code

--- tensornetwork/matrixproductstates/mps.py
+++ tensornetwork/matrixproductstates/mps.py
@@ -145,12 +145,8 @@ class FiniteMPS(TensorNetwork):
             left_edges=[self._nodes[n][0]],
             right_edges=[self._nodes[n][1], self._nodes[n][2]],
             right_name=self._nodes[n].name)
-        Z = self.backend.norm(R.tensor)
-
         # for an mps with > O(10) sites one needs to normalize to avoid
         # over or underflow errors; this takes care of the normalization
-        if normalize:
-          R.tensor /= Z
         self._nodes[n] = Q  #Q is a right-isometric tensor of rank 3
         self._nodes[n - 1] = self.contract(R[0], name=self._nodes[n - 1].name)
         Z = self.backend.norm(self._nodes[n - 1].tensor)
@@ -159,7 +155,7 @@ class FiniteMPS(TensorNetwork):
 
       self.center_position = site
     #return the norm of the last R tensor (useful for checks)
-    return Z
+    #return Z
 
   def check_orthonormality(self, which: Text, site: int) -> Tensor:
     """
